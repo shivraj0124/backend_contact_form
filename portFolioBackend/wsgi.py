3ef0224d7689b86eba228aece9ea9540a52c1123
shivraj <sk40fs@gmail.com>
2023-12-19 00:37:20 +0530

    'new2'

--- portFolioBackend/wsgi.py
+++ portFolioBackend/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portFolioBackend.settings')
+
+# application = get_wsgi_application()
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portFolioBackend.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saveplate.settings')
 application = get_wsgi_application()
+app = application #add here
