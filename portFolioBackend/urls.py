512258916756674c912667d4938820723f70f154
shivraj <sk40fs@gmail.com>
2023-12-19 00:14:42 +0530

    'new'

--- portFolioBackend/urls.py
+++ portFolioBackend/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('api/', include('contact_form.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
